21ac24d660089c413fe6b5942f98b9b6994cc398
Vedran Mandić <mandic.vedran@gmail.com>
2018-12-13 15:11:38 +0100

    minor refactoring

--- src/EfQueryableExtensions/EfQueryableExtensions.cs
+++ src/EfQueryableExtensions/EfQueryableExtensions.cs
@@ -1,22 +1,22 @@
-using Microsoft.EntityFrameworkCore.Query;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
+using Microsoft.EntityFrameworkCore.Query;
 
 namespace EfQueryableExtensions
 {
+  public enum SortOrder
+  {
+    Ascending = 0,
+    Descending = 1
+  }
+
   public static class EfQueryableExtensions
   {
     const int PAGE_SIZE_DEFAULT = 12;
 
-    public enum SortOrder
-    {
-      Ascending = 0,
-      Descending = 1
-    }
-
-    public static (IQueryable<T> pagedQuery, Func<int> countTotal) QueryPage<T>(
+    public static(IQueryable<T> pagedQuery, Func<int> countTotal) QueryPage<T>(
       this IQueryable<T> baseQuery,
       int? page = 1,
       int? size = PAGE_SIZE_DEFAULT,
@@ -30,14 +30,17 @@ namespace EfQueryableExtensions
       if (!size.HasValue)
         throw new ArgumentNullException(nameof(size));
 
-      var pagedQuery = Paginate(baseQuery, size.Value, (page.Value - 1) * size.Value);
-      var pagedFilteredAndIncludedQuery = FilterAndInclude(pagedQuery, filter, includes);
-      int countTotal() => FilterAndInclude(baseQuery, filter, includes).Count();
+      var filteredAndIncludedQuery = FilterAndInclude(baseQuery, filter, includes);
+
+      var pagedFilteredAndIncludedQuery =
+        Paginate(filteredAndIncludedQuery, size.Value, (page.Value - 1) * size.Value);
+
+      int countTotal() => filteredAndIncludedQuery.Count();
 
       return (pagedQuery: pagedFilteredAndIncludedQuery, countTotal);
     }
 
-    public static (IQueryable<ProjectT> pagedQuery, Func<int> countTotal) QueryPage<T, ProjectT>(
+    public static(IQueryable<ProjectT> pagedQuery, Func<int> countTotal) QueryPage<T, ProjectT>(
       this IQueryable<T> baseQuery,
       int? page = 1,
       int? size = PAGE_SIZE_DEFAULT,
@@ -46,30 +49,34 @@ namespace EfQueryableExtensions
       Expression<Func<T, ProjectT>> project = null
     ) where T : class
     {
-      var (pagedQuery, countTotal) = QueryPage(baseQuery, page, size, filter, includes);
+      var (pagedQuery, countTotal) =
+      QueryPage(baseQuery, page, size, filter, includes);
+
       return (pagedQuery.Select(project), countTotal);
     }
 
-    public static (IQueryable<ProjectT> pagedQuery, Func<int> countTotal) QueryPage<T, ProjectT>(
+    public static(IQueryable<ProjectT> pagedQuery, Func<int> countTotal) QueryPage<T, ProjectT>(
       this IQueryable<T> baseQuery,
       int? page = 1,
       int? size = PAGE_SIZE_DEFAULT,
       Expression<Func<T, bool>> filter = null,
       Func<IQueryable<T>, IIncludableQueryable<T, object>> includes = null,
       Expression<Func<T, ProjectT>> project = null,
-      params KeyValuePair<Expression<Func<T, object>>, SortOrder>[] orderBys
+      params KeyValuePair<Expression<Func<T, object>>, SortOrder >[] orderBys
     ) where T : class
     {
       var sortedQuery = Sort(baseQuery, orderBys);
-      var (sortedPagedFilteredAndIncludedQuery, countTotal) = QueryPage(sortedQuery, page, size, filter, includes);
+
+      var (sortedPagedFilteredAndIncludedQuery, countTotal) =
+      QueryPage(sortedQuery, page, size, filter, includes);
 
       return (pagedQuery: sortedPagedFilteredAndIncludedQuery.Select(project), countTotal);
     }
 
     private static IQueryable<T> Paginate<T>(IQueryable<T> baseQuery, int size, int skipCount) =>
-      skipCount == 0
-        ? baseQuery.Take(size)
-        : baseQuery.Skip(skipCount).Take(size);
+      skipCount == 0 ?
+      baseQuery.Take(size) :
+      baseQuery.Skip(skipCount).Take(size);
 
     private static IQueryable<T> FilterAndInclude<T>(
       IQueryable<T> baseQuery,
@@ -78,27 +85,31 @@ namespace EfQueryableExtensions
     {
       var queryIncluded = Include(baseQuery, includes);
 
-      return filter != null
-        ? queryIncluded.Where(filter)
-        : queryIncluded;
+      return filter != null ?
+        queryIncluded.Where(filter) :
+        queryIncluded;
     }
 
-    private static IQueryable<T> Sort<T>(IQueryable<T> baseQuery, KeyValuePair<Expression<Func<T, object>>, SortOrder>[] orderBys)
+    private static IQueryable<T> Sort<T>(
+      IQueryable<T> baseQuery,
+      KeyValuePair<Expression<Func<T, object>>, SortOrder >[] orderBys)
     {
       if (orderBys != null && orderBys.Length > 0)
       {
         foreach (var ordering in orderBys)
         {
-          baseQuery = ordering.Value == SortOrder.Ascending
-            ? baseQuery.OrderBy(ordering.Key)
-            : baseQuery.OrderByDescending(ordering.Key);
+          baseQuery = ordering.Value == SortOrder.Ascending ?
+            baseQuery.OrderBy(ordering.Key) :
+            baseQuery.OrderByDescending(ordering.Key);
         }
       }
 
       return baseQuery;
     }
 
-    private static IQueryable<T> Include<T>(IQueryable<T> baseQuery, Func<IQueryable<T>, IIncludableQueryable<T, object>> includes) =>
+    private static IQueryable<T> Include<T>(
+        IQueryable<T> baseQuery,
+        Func<IQueryable<T>, IIncludableQueryable<T, object>> includes) =>
       includes?.Invoke(baseQuery) ?? baseQuery;
   }
 }
